7c80dc6ddd4d7dc3f901868406301bff6af85c32
Eichenherz <pelesliviu@yahoo.com>
2017-09-23 18:54:16 +0300

    Fixed errors & added test. Vec V1.0

--- DataStructs and Algorithms/Vector.h
+++ DataStructs and Algorithms/Vector.h
@@ -1,23 +1,31 @@
 #pragma once
 
+//////////////////////////////////////
+////******************************////
+////       Vector CONTAINER       ////
+////******************************////
+//////////////////////////////////////
+////  TODO: add assertions & 
+////                  extra polish
+//////////////////////////////////////
 template<typename T>
 class Vector
 {
 public:
-	typedef T* iterator;
+	typedef		  T* iterator;
 	typedef const T* const_iterator;
-	/*
-	*
-	*/
+	/**
+	**
+	**/
 	explicit Vector( int size = 0 );
 			 Vector( const Vector& src );
 			 Vector( Vector&& src );
 			~Vector();
 
-	Vector&  operator=( const Vector& src );
-	Vector&  operator=( Vector&& src );
-	T&		 operator[]( const int index );
-	const T& operator[]( const int index ) const;
+	Vector<T>&  operator=( const Vector& src );
+	Vector<T>&  operator=( Vector&& src );
+	T&			operator[]( const int index );
+	const T&	operator[]( const int index ) const;
 
 
 	void	 resize( int n_size );
@@ -31,14 +39,14 @@ public:
 	int		 capacity() const;
 
 	iterator		begin();
-	const_iterator	begin();
+	const_iterator	c_begin();
 	iterator		end();
-	const_iterator	end();
+	const_iterator	c_end();
 
 private:
 	T*						p_objects = nullptr;
-	int						capacity;
-	int						size;
+	int						cap;
+	int						lenght;
 	static constexpr int	SPARE_CAP = 16;
 };
 
@@ -48,29 +56,29 @@ private:
 template<typename T>
 inline Vector<T>::Vector( int size )
 	:
-	size	 { size },
-	capacity { size + SPARE_CAP }
+	lenght { size },
+	cap	   { size + SPARE_CAP }
 {
-	p_objects = new T [capacity];
+	p_objects = new T [cap];
 }
 
 template<typename T>
 inline Vector<T>::Vector( const Vector & src )
 	:
-	size	 { src.size },
-	capacity { src.capacity }
+	lenght { src.lenght },
+	cap	   { src.cap }
 {
-	p_objects = new T[ capacity ];
+	p_objects = new T[ cap ];
 
-	for ( int i = 0; i < size; ++i )
+	for ( int i = 0; i < lenght; ++i )
 		this [i] = src [i];
 }
 
 template<typename T>
 inline Vector<T>::Vector( Vector && src )
 	:
-	size	  { src.size },
-	capacity  { src.capacity },
+	lenght	  { src.lenght },
+	cap		  { src.cap },
 	p_objects { src.p_objects }
 {
 	src.p_objects = nullptr;
@@ -84,25 +92,25 @@ inline Vector<T>::~Vector()
 }
 
 template<typename T>
-inline Vector& Vector<T>::operator=( const Vector & src )
+inline Vector<T>& Vector<T>::operator=( const Vector & src )
 {
-	this->size = src.size;
-	this->capacity = src.capacity;
+	this->lenght = src.lenght;
+	this->cap = src.cap;
 
 	delete[] p_objects;
-	p_objects = new T [capacity];
+	p_objects = new T [cap];
 
-	for ( int i = 0; i < size; ++i )
+	for ( int i = 0; i < lenght; ++i )
 		this [i] = src [i];
 
 	return *this;
 }
 
 template<typename T>
-inline Vector& Vector<T>::operator=( Vector && src )
+inline Vector<T>& Vector<T>::operator=( Vector && src )
 {
-	this->size = src.size;
-	this->capacity = src.capacity;
+	this->lenght = src.lenght;
+	this->cap = src.cap;
 
 	delete[] p_objects;
 	this->p_objects = src.p_objects;
@@ -126,21 +134,21 @@ inline const T & Vector<T>::operator[]( const int index ) const
 template<typename T>
 inline void Vector<T>::resize( int n_size )
 {
-	if ( n_size > capacity )
+	if ( n_size > cap )
 		reserve( 2 * n_size );
-	size = n_size;
+	lenght = n_size;
 }
 
 template<typename T>
 inline void Vector<T>::reserve( int n_cap )
 {
-	if ( n_cap < size ) return;
+	if ( n_cap < lenght ) return;
 
 	T* array = new T [n_cap];
-	for ( int i = 0; i < size; ++i )
+	for ( int i = 0; i < lenght; ++i )
 		array [i] = p_objects [i];
 
-	capacity = n_cap;
+	cap = n_cap;
 
 	delete[] p_objects;
 	p_objects = array;
@@ -150,67 +158,67 @@ inline void Vector<T>::reserve( int n_cap )
 template<typename T>
 inline void Vector<T>::push_back( const T & obj )
 {
-	if ( size == capacity )
-		reserve( 2 * capacity + 1 );
-	p_objects [size++] = obj;
+	if ( lenght == cap )
+		reserve( 2 * cap + 1 );
+	p_objects [lenght++] = obj;
 }
 
 template<typename T>
 inline void Vector<T>::pop_back()
 {
-	--size;
+	--lenght;
 }
 
 template<typename T>
 inline T & Vector<T>::back()
 {
-	return p_objects [size];
+	return p_objects [lenght - 1];
 }
 
 template<typename T>
 inline const T & Vector<T>::back() const
 {
-	return p_objects [size];
+	return p_objects [lenght];
 }
 
 template<typename T>
 inline bool Vector<T>::empty() const
 {
-	return (size == 0);
+	return ( lenght == 0);
 }
 
 template<typename T>
 inline int Vector<T>::size() const
 {
-	return size;
+	return lenght;
 }
 
 template<typename T>
 inline int Vector<T>::capacity() const
 {
-	return capacity;
+	return cap;
 }
 
 template<typename T>
-inline Vector<T>::iterator Vector<T>::begin()
+inline typename Vector<T>::iterator Vector<T>::begin()
 {
 	return &( p_objects [0] );
 }
 
 template<typename T>
-inline Vector<T>::const_iterator Vector<T>::begin()
+inline typename Vector<T>::const_iterator Vector<T>::c_begin()
 {
 	return &( p_objects [0] );
 }
 
 template<typename T>
-inline Vector<T>::iterator Vector<T>::end()
+inline typename Vector<T>::iterator Vector<T>::end()
 {
-	return &( p_objects [size + 1] );
+	return &( p_objects [lenght + 1] );
 }
 
 template<typename T>
-inline Vector<T>::const_iterator Vector<T>::end()
+inline typename Vector<T>::const_iterator Vector<T>::c_end()
 {
-	return &( p_objects [size + 1] );
+	return &( p_objects [sze + 1] );
 }
