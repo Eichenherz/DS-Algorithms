62d77ba98839c38d921108e111924901772984ae
Eichenherz <pelesliviu@yahoo.com>
2018-07-23 15:01:07 +0300

    Merge_Sort sorted

--- DataStructs and Algorithms/Sort.h
+++ DataStructs and Algorithms/Sort.h
@@ -3,50 +3,72 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <iostream>
 
-template<typename T>
-std::vector<T> Merge( std::vector<T>& v, std::vector<T>& u )
+template<class Iter>
+void Merge_Sort_Recursive( Iter src_beg, Iter src_end, 
+						   Iter trg_beg, Iter trg_end )
 {
-	std::vector<T> m;
-	size_t i = 0, j = 0;
-	while ( ( i != v.size() ) && ( j != u.size() ) )
+	auto range_lenght = std::distance( src_beg, src_end );
+	if ( range_lenght < 2 )
 	{
-		if ( v[i] < u[j] )
-		{
-			m.push_back( v [i] );
-			++i;
-		}
-		else
-		{
-			m.push_back( u [j] );
-			++j;
-		}
+		return;
 	}
 
-	for ( ; i != v.size(); ++i ) m.push_back( v [i] );
-	for ( ; j != u.size(); ++j ) m.push_back( u [j] );
+	auto mid_pt = range_lenght >> 1;
+	Iter src_mid = src_beg;
+	std::advance( src_mid, mid_pt );
+	Iter trg_mid = trg_beg;
+	std::advance( trg_mid, mid_pt );
 
-	return m;
+	std::cout << "\n";
+	print_test( src_beg, src_mid, src_end, trg_beg, trg_mid, trg_end );
+
+	Merge_Sort_Recursive( trg_beg, trg_mid, src_beg, src_mid );
+	Merge_Sort_Recursive( trg_mid, trg_end, src_mid, src_end );
+
+	std::merge( src_beg, src_mid, 
+				src_mid, src_end,
+				trg_beg );
+
+	std::cout << "\nMerged:\n";
+	print_test( src_beg, src_mid, src_end, trg_beg, trg_mid, trg_end );
 }
 
-template<typename T>
-std::vector<T>& Merge_Sort( std::vector<T>& v )
+template<class Iter>
+void Merge_Sort( Iter beg, Iter end )
 {
-	size_t mid = v.size() / 2;
-	if ( mid > 0 )
+	using T = typename std::iterator_traits<Iter>::value_type;
+	std::vector<T> aux( beg, end );
+
+	Merge_Sort_Recursive( aux.begin(), aux.end(), beg, end );
+}
+
+template<class Iter>
+void print_test( Iter src_beg, Iter src_mid, Iter src_end,
+				 Iter trg_beg, Iter trg_mid, Iter trg_end )
+{
+	std::cout << "Src : " << "\n";
+	for ( auto temp = src_beg; temp != src_mid; ++temp )
 	{
-		std::vector<T> left_half, right_half; // use std::move for expensive types
-		std::copy( std::begin( v ), std::begin( v ) + mid, std::back_inserter( left_half ) );
-		std::copy( std::begin( v ) + mid, std::end( v ), std::back_inserter( right_half ) );
-
-		left_half = Merge_Sort( left_half );
-		right_half = Merge_Sort( right_half );
-
-		v = Merge( left_half, right_half );
-
-		return v;
+		std::cout << *temp << ' ';
 	}
-	else return v;
+	std::cout << ' ' << "Mid :" << *src_mid << ' '<<' ';
+	for ( auto temp = src_mid+1; temp != src_end; ++temp )
+	{
+		std::cout << *temp << ' ';
+	}
+	std::cout << std::endl;
 
-
+	std::cout << "Trg : " << "\n";
+	for ( auto temp = trg_beg; temp != trg_mid; ++temp )
+	{
+		std::cout << *temp << ' ';
+	}
+	std::cout << ' ' << "Mid :" << *trg_mid << ' '<<' ';
+	for ( auto temp = trg_mid + 1; temp != trg_end; ++temp )
+	{
+		std::cout << *temp << ' ';
+	}
+	std::cout << std::endl;
 }
--- DataStructs and Algorithms/main.cpp
+++ DataStructs and Algorithms/main.cpp
@@ -12,30 +12,32 @@ using namespace std;
 
 int main()
 {
-	std::vector<int> test;
+	std::vector<int> test {1,0,0,-12,64,2,5,-88};
 
+	/*
 	std::mt19937 rng( std::random_device {}( ) );
 	std::uniform_int_distribution<int> dist;
-	int n_obj = 10;
+	int n_obj = 8;
 	if ( n_obj % 2 == 0 )
 	{
-		std::uniform_int_distribution<int> dist( -n_obj / 2, n_obj / 2 );
-		for ( ; n_obj != 0; --n_obj )
-			test.push_back( dist( rng ) );
+	std::uniform_int_distribution<int> dist( -n_obj / 2, n_obj / 2 );
+	for ( ; n_obj != 0; --n_obj )
+	test.push_back( dist( rng ) );
 	}
 
 	else
 	{
-		std::uniform_int_distribution<int> dist( -( n_obj + 1 ) / 2, n_obj / 2 );
-		for ( ; n_obj != 0; --n_obj )
-			test.push_back( dist( rng ) );
+	std::uniform_int_distribution<int> dist( -( n_obj + 1 ) / 2, n_obj / 2 );
+	for ( ; n_obj != 0; --n_obj )
+	test.push_back( dist( rng ) );
 	}
+	*/
 
 	for ( int e : test )
 		std::cout << e << ' ';
 	std::cout << std::endl;
 
-	test = Merge_Sort( test );
+	Merge_Sort( test.begin(), test.end() );
 
 	for ( int e : test )
 		std::cout << e << ' ';
