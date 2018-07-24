4b94660efa23675da452cff8040cb808ec094a34
Eichenherz <pelesliviu@yahoo.com>
2018-07-24 18:32:35 +0300

    Sorted

--- DataStructs and Algorithms/Sort.h
+++ DataStructs and Algorithms/Sort.h
@@ -5,6 +5,28 @@
 #include <iterator>
 #include <iostream>
 
+template<class InputIt1, class InputIt2, class OutputIt>
+OutputIt merge_routine( InputIt1 first1, InputIt1 last1,
+				InputIt2 first2, InputIt2 last2,
+				OutputIt d_first )
+{
+	for ( ; first1 != last1; ++d_first ) {
+		if ( first2 == last2 ) {
+			return std::copy( first1, last1, d_first );
+		}
+		if ( *first2 < *first1 ) {
+			*d_first = *first2;
+			++first2;
+		}
+		else {
+			*d_first = *first1;
+			++first1;
+		}
+	}
+	return std::copy( first2, last2, d_first );
+}
+
+
 template<class Iter>
 void Merge_Sort_Recursive( Iter src_beg, Iter src_end, 
 						   Iter trg_beg, Iter trg_end )
@@ -27,7 +49,7 @@ void Merge_Sort_Recursive( Iter src_beg, Iter src_end,
 	Merge_Sort_Recursive( trg_beg, trg_mid, src_beg, src_mid );
 	Merge_Sort_Recursive( trg_mid, trg_end, src_mid, src_end );
 
-	std::merge( src_beg, src_mid, 
+	merge_routine( src_beg, src_mid, 
 				src_mid, src_end,
 				trg_beg );
 
